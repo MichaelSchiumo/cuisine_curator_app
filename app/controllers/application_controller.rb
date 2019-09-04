a2d5bf8437d77a2686b208683fc76d098e36c596
Michael Schiumo <mschiumo18@gmail.com>
2019-09-04 14:22:14 -0400

    added current_user and require_login helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    if session[:user_id].present?
+      user = User.find_by(:id => session[:user_id])
+    end
+  end
+
+  def require_login
+    unless current_user
+      redirect_to root_path
+    end
+  end    
 end
