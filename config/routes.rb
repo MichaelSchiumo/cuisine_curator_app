efd561e5524e2e6320a993b56a780d802b334a29
Michael Schiumo <mschiumo18@gmail.com>
2019-09-07 12:57:14 -0400

    added nested routes for cuisine and meals

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cuisines do
+    resources :meals, only: [:new, :create, :index, :show]
+  end
+
   root 'welcome#home'
   resources :users
-  resoures :sessions
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  # delete '/signout' => 'sessions#destroy'
+  delete '/signout' => 'sessions#destroy'
+
 end
