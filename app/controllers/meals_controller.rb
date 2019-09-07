96cf22b1db7b1c3d3cece29dec5bbb8323f242c2
Michael Schiumo <mschiumo18@gmail.com>
2019-09-07 14:12:29 -0400

    added before action and private method

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,10 @@
 class MealsController < ApplicationController
+  before_action :set_meal, only: [:edit, :show, :update]
+
+
+
+  private
+    def meal_params
+      params.require(:meal).require(:type, :difficulty, :rating, :user_id, :cuisine_id, :name)
+    end  
 end
