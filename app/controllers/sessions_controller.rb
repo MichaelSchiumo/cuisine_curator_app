f4a1f7391a574e3821f7852d0ca2dd61439e7d59
Michael Schiumo <mschiumo18@gmail.com>
2019-09-09 17:39:41 -0400

    added create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,23 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create_from_omniauth
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = 'auth'['info']['name']
+      u.password = SecureRandom.hex
+    end
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)  
+  end
+
   def destroy
     session.destroy
     redirect_to root_path
   end
+
+  private
+
+    def auth
+      request.env['omniauth.auth']
+    end
 end
