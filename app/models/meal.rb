cb86af6c8918368ad9bb5a48387e6135f198563c
Michael Schiumo <mschiumo18@gmail.com>
2019-09-10 23:32:08 -0400

    added validation for difficulty

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,6 @@
 class Meal < ApplicationRecord
   validates :rating, inclusion: { in: 1..10, message: 'Must be between 1 and 10' }
+  validates :difficulty, inclusion: { in: 1..5, message: 'Must be between 1 and 5' }
   #validates :presence
   belongs_to :cuisine
   belongs_to :user
