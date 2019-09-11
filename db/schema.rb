ca8010f5299e74180bc020a45e8ea9f333c1de32
Michael Schiumo <mschiumo18@gmail.com>
2019-09-11 12:11:07 -0400

    added tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_191104) do
+ActiveRecord::Schema.define(version: 2019_09_11_160844) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "category"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_09_07_191104) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "google_token"
+    t.string "google_refresh_token"
   end
 
 end
