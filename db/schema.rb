251bafd6eb1fceb76a120e988202b83833c37a3e
Michael Schiumo <mschiumo18@gmail.com>
2019-09-11 15:11:29 -0400

    created ingredients table/model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_160844) do
+ActiveRecord::Schema.define(version: 2019_09_11_190921) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "category"
     t.string "classification"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
