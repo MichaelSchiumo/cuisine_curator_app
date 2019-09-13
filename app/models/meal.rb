2d581535db73bc5d8640f6a465f37102c4194917
Michael Schiumo <mschiumo18@gmail.com>
2019-09-13 11:05:28 -0400

    added data validations for difficulty attribute

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,6 @@
 class Meal < ApplicationRecord
   validates :rating, inclusion: { in: 1..10, message: 'Must be between 1 and 10' }
-  # validates :difficulty, inclusion: { in: 1..5, message: 'Must be between 1 and 5' }
+  validates :difficulty, inclusion: { in: 1..5, message: 'Must be between 1 and 5' }
   validates :name, presence: true
   validates :ingredients, presence: true
   validates :course, presence: true
