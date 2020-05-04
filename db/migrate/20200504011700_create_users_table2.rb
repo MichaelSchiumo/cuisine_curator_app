a77076c48dbfc9fa7d08c107e7ea5e7977d6b3cc
Michael Schiumo <mschiumo18@gmail.com>
2020-05-03 21:18:44 -0400

    remigrated

---
+++ db/migrate/20200504011700_create_users_table2.rb
@@ -0,0 +1,12 @@
+class CreateUsersTable2 < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string "name"
+      t.string "password_digest"
+      t.string "uid"
+      t.integer "years_experience"
+      t.string "email"
+      t.string "location"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_011309) do
+ActiveRecord::Schema.define(version: 2020_05_04_011700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,6 @@ ActiveRecord::Schema.define(version: 2020_05_04_011309) do
     t.integer "years_experience"
     t.string "email"
     t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "google_token"
-    t.string "google_refresh_token"
   end
 
 end
