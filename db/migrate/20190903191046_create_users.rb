08e2bc729cbb16a06065046c54fac1a8c735685b
Michael Schiumo <mschiumo18@gmail.com>
2020-05-03 21:06:51 -0400

    added app_user to table

--- db/migrate/20190903191046_create_users.rb
+++ db/migrate/20190903191046_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
+    create_table :app_users do |t|
       t.string :name
       t.string :password_digest
       t.string :uid
